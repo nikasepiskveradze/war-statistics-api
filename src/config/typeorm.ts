5546b4c6f93d22abe4c2b9a56535855a72afe40a
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2023-10-04 17:10:11 +0400

    Bring back

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,12 +7,11 @@ dotenvConfig({ path: '.env' });
 
 const config = {
   type: 'postgres',
-  // host: `${process.env.POSTGRES_HOST}`,
-  // port: `${process.env.POSTGRES_PORT}`,
-  // username: `${process.env.POSTGRES_USER}`,
-  // password: `${process.env.POSTGRES_PASSWORD}`,
-  // database: `${process.env.POSTGRES_DB}`,
-  url: `${process.env.DATABASE_URL}`,
+  host: `${process.env.POSTGRES_HOST}`,
+  port: `${process.env.POSTGRES_PORT}`,
+  username: `${process.env.POSTGRES_USER}`,
+  password: `${process.env.POSTGRES_PASSWORD}`,
+  database: `${process.env.POSTGRES_DB}`,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
